770e8db54200e9295ef5580935c72ef09978b019
rifatrakib <Rakib.1508@outlook.com>
2022-05-20 00:59:10 +0600

    created app for repair shops, problems in migration for os config need to be solved

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'property_sales.apps.PropertySalesConfig',
+    # 'property_sales.apps.PropertySalesConfig',
+    # 'repair_shops.apps.RepairShopsConfig',
 ]
 
 MIDDLEWARE = [
