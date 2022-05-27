5d13c8da748adf967eaf5ea1a7898ceec8b64d39
rifatrakib <Rakib.1508@outlook.com>
2022-05-28 01:51:02 +0600

    admin access only route made, to be used for populating database table

--- repair_shops/views.py
+++ repair_shops/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from core.utils import process_automobile_data
+from core.decorators import admin_only
 
-# Create your views here.
+
+@admin_only
+def build_automobile_dataframe(request, segment):
+    df = process_automobile_data(segment)
+    print(df.to_html())
+    return render(request, 'repair_shops/index.html')
