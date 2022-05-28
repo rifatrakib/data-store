20e551c45276aea60d934ab71c1445df5717271b
rifatrakib <Rakib.1508@outlook.com>
2022-05-28 18:20:49 +0600

    admin only route successfully populates table, 10000 per page

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 
 def process_automobile_data(segment_number):
@@ -6,6 +7,15 @@ def process_automobile_data(segment_number):
     end = segment_number * 10000
     directory = f'raw-data/csv/automobile/automobile-data-{start}-{end}.csv'
     df = pd.read_csv(directory, sep='|')
+    
     df['license_expiration'] = df['license_expiration'].astype('datetime64')
-    df = df.rename(columns={'id': 'shop_id'}).drop(columns=['Unnamed: 0'])
-    return df
+    df = df.rename(columns={'id': 'shop_number'}).drop(columns=['Unnamed: 0'])
+    
+    point_condition = df['geo_coordinates'].notna()
+    df['geo_coordinates'] = df['geo_coordinates'].str.replace('\'', '\"')
+    df['geo_coordinates'] = df[point_condition]['geo_coordinates'].apply(json.loads).map(lambda x: x[0])
+    df['geo_type'] = df[point_condition]['geo_coordinates'].map(lambda x: x['type']).str.upper()
+    df['lon'] = df[point_condition]['geo_coordinates'].map(lambda x: x['coordinates'][0])
+    df['lat'] = df[point_condition]['geo_coordinates'].map(lambda x: x['coordinates'][1])
+    
+    return df.to_dict(orient='records')
--- repair_shops/views.py
+++ repair_shops/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from core.utils import process_automobile_data
 from core.decorators import admin_only
-
+from django.contrib.gis.geos import Point
+from core.utils import process_automobile_data
+from repair_shops.models import AutomobileRepairShop
 
 @admin_only
 def build_automobile_dataframe(request, segment):
-    df = process_automobile_data(segment)
+    shops = process_automobile_data(segment)
+    for shop in shops:
+        geo_type = shop.get('geo_type', None)
+        lon = shop.get('lon', None)
+        lat = shop.get('lat', None)
+        point_data = Point(lon, lat) if geo_type else None
+        AutomobileRepairShop.objects.create(
+            shop_number=shop.get('shop_number', None),
+            business_name=shop.get('business_name', None),
+            business_address=shop.get('business_address', None),
+            city=shop.get('city', None),
+            state=shop.get('state', None),
+            zip_code=shop.get('zip_code', None),
+            license_num=shop.get('license_num', None),
+            license_type=shop.get('license_type', None),
+            license_expiration=shop.get('license_expiration', None),
+            location=point_data,
+        )
     return render(request, 'repair_shops/index.html')
