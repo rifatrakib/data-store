b095ced84e79010d110abae593c27ddbe1f24ca5
rifatrakib <Rakib.1508@outlook.com>
2022-06-07 07:03:05 +0600

    csv to pandas dataframe parsing logic and minimal processing for small businesses

--- core/utils.py
+++ core/utils.py
@@ -35,3 +35,78 @@ def process_property_sales_data(segment_number):
     df['geo_coordinates'] = df[point_condition]['geo_coordinates'].apply(json.loads).map(lambda x: x[0])
     
     return df.to_dict(orient='records')
+
+
+def process_small_business_data(segment_number):
+    start = (segment_number - 1) * 10000
+    end = segment_number * 10000
+    directory = f'raw-data/csv/business/business-data-{start}-{end}.csv'
+    
+    dtypes = {
+        'account_number': 'str',
+        'annual_report_due_date': 'str',
+        'began_transacting_in_ct': 'str',
+        'business_city': 'str',
+        'business_country': 'str',
+        'business_email_address': 'str',
+        'business_name_in_state_country': 'str',
+        'business_state': 'str',
+        'business_street': 'str',
+        'business_type': 'str',
+        'business_unit': 'str',
+        'business_zip_code': 'str',
+        'category_survey_email_address': 'str',
+        'citizenship': 'str',
+        'country_formation': 'str',
+        'created_on': 'str',
+        'date_of_organization_meeting': 'str',
+        'disable_person_owned_organization': 'bool',
+        'dissolution_date': 'str',
+        'formation_place': 'str',
+        'id': 'str',
+        'mail_jurisdiction': 'str',
+        'mailing_address': 'str',
+        'mailing_international_address': 'str',
+        'mailing_jurisdiction_address': 'str',
+        'mailing_jurisdiction_business_city': 'str',
+        'mailing_jurisdiction_business_country': 'str',
+        'mailing_jurisdiction_business_state': 'str',
+        'mailing_jurisdiction_business_street': 'str',
+        'mailing_jurisdiction_business_unit': 'str',
+        'mailing_jurisdiction_business_zip_code': 'str',
+        'minority_owned_organization': 'bool',
+        'naics_code': 'str',
+        'naics_sub_code': 'str',
+        'name': 'str',
+        'office_jurisdiction_address': 'str',
+        'office_jurisdiction_business_city': 'str',
+        'office_jurisdiction_business_state': 'str',
+        'office_jurisdiction_business_street': 'str',
+        'office_jurisdiction_business_unit': 'str',
+        'office_jurisdiction_business_zip_code': 'str',
+        'office_jurisdiction_country': 'str',
+        'reason_for_administrative_dissolution': 'str',
+        'record_address': 'str',
+        'records_address_city': 'str',
+        'records_address_country': 'str',
+        'records_address_state': 'str',
+        'records_address_street': 'str',
+        'records_address_unit': 'str',
+        'records_address_zip_code': 'str',
+        'registration_date': 'str',
+        'state_or_territory_formation': 'str',
+        'status': 'str',
+        'sub_status': 'str',
+        'total_authorized_shares': 'Int64',
+        'veteran_owned_organization': 'bool',
+        'woman_owned_organization': 'bool',
+    }
+    
+    date_fields = [
+        'annual_report_due_date', 'began_transacting_in_ct', 'created_on',
+        'date_of_organization_meeting', 'dissolution_date', 'registration_date'
+    ]
+    
+    df = pd.read_csv(directory, sep='|', dtype=dtypes, parse_dates=date_fields)
+    df = df.rename(columns={'id': 'business_identifier'}).drop(columns=['Unnamed: 0'])
+    return df.to_dict(orient='records')
