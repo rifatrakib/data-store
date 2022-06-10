302cd9eac6e216f741145e92742dd17302729372
rifatrakib <Rakib.1508@outlook.com>
2022-06-11 00:58:49 +0600

    scraping friendly public routes made for small business data

--- businesses/urls.py
+++ businesses/urls.py
@@ -3,6 +3,8 @@ from businesses.views import *
 
 app_name = 'businesses'
 urlpatterns = [
+    path('business-record/<int:business_id>/json/', get_small_business_as_json, name='business_record_json'),
+    path('business-record/<int:business_id>/xml/', get_repair_shops_as_xml, name='business_record_xml'),
     path('populate-business-record/', generate_page_numbers, name='business_admin_home'),
     path('populate-business-record/<int:segment>/', build_small_business_data, name='populate_business_record'),
 ]
