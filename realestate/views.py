8e288f55a0dc7e51375cd979bcfe29ce5e2c2073
rifatrakib <Rakib.1508@outlook.com>
2022-07-22 19:45:20 +0600

    view added for geojson property data

--- realestate/views.py
+++ realestate/views.py
@@ -1,3 +1,22 @@
+import json
+from datetime import datetime
 from django.shortcuts import render
+from django.core import serializers
+from django.http import JsonResponse
+from realestate.models import Property
 
-# Create your views here.
+
+def get_property_as_json(request, property_id):
+    property_record = Property.objects.filter(pk=property_id)
+    data = json.loads(serializers.serialize('geojson', property_record))
+    response = {
+        'source': request.build_absolute_uri(),
+        'headers': dict(request.headers),
+        'api': 'public',
+        'identifier': property_id,
+        'success': True,
+        'data': data,
+        'format': 'text/json',
+        'timestamp': str(datetime.utcnow()) + ' UTC',
+    }
+    return JsonResponse(response)
