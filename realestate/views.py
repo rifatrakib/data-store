2d4a77ace14fe41047e5ddc3f9343cd0b5da924c
rifatrakib <Rakib.1508@outlook.com>
2022-07-22 19:48:27 +0600

    view function added for geojson building response

--- realestate/views.py
+++ realestate/views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.shortcuts import render
 from django.core import serializers
 from django.http import JsonResponse
-from realestate.models import Property
+from realestate.models import Property, Building
 
 
 def get_property_as_json(request, property_id):
@@ -14,6 +14,22 @@ def get_property_as_json(request, property_id):
         'headers': dict(request.headers),
         'api': 'public',
         'identifier': property_id,
+        'success': True,
+        'data': data,
+        'format': 'text/json',
+        'timestamp': str(datetime.utcnow()) + ' UTC',
+    }
+    return JsonResponse(response)
+
+
+def get_building_as_json(request, building_id):
+    building_record = Building.objects.filter(pk=building_id)
+    data = json.loads(serializers.serialize('geojson', building_record))
+    response = {
+        'source': request.build_absolute_uri(),
+        'headers': dict(request.headers),
+        'api': 'public',
+        'identifier': building_id,
         'success': True,
         'data': data,
         'format': 'text/json',
