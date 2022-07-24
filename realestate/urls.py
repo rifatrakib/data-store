abd8427c3ae6ef1506a9d4b76f2fb6373c118c94
rifatrakib <Rakib.1508@outlook.com>
2022-07-24 09:11:36 +0600

    added url routes to access property and building data as xml

--- realestate/urls.py
+++ realestate/urls.py
@@ -3,6 +3,11 @@ from realestate.views import *
 
 app_name = 'realestate'
 urlpatterns = [
+    # property links
     path('realestate/property/<int:property_id>/json/', get_property_as_json, name='property_json'),
+    path('realestate/property/<int:property_id>/xml/', get_property_as_xml, name='property_xml'),
+    
+    # building links
     path('realestate/building/<int:building_id>/json/', get_building_as_json, name='building_json'),
+    path('realestate/building/<int:building_id>/xml/', get_building_as_xml, name='building_xml'),
 ]
