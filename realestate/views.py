36aea98348df22b5a2026af3468cf211bd461b66
rifatrakib <Rakib.1508@outlook.com>
2022-07-25 09:02:19 +0600

    untested views added for building and property pages as html

--- realestate/views.py
+++ realestate/views.py
@@ -6,6 +6,22 @@ from realestate.models import Property, Building
 from django.http import HttpResponse, JsonResponse
 
 
+# not tested yet
+def get_property_page(request, page=1):
+    start, end = (page-1) * 100, page * 100
+    properties = Property.objects.all().order_by('id')[start:end]
+    total_pages = Property.objects.all().count() // 100
+    return render(request, 'realestate/properties.html', {'properties': properties, 'total_pages': range(1, total_pages+1)})
+
+
+# not tested yet
+def get_building_page(request, page=1):
+    start, end = (page-1) * 100, page * 100
+    buildings = Building.objects.all().order_by('id')[start:end]
+    total_pages = Building.objects.all().count() // 100
+    return render(request, 'realestate/buildings.html', {'buildings': buildings, 'total_pages': range(1, total_pages+1)})
+
+
 def get_property_as_json(request, property_id):
     property_record = Property.objects.filter(pk=property_id)
     data = json.loads(serializers.serialize('geojson', property_record))
