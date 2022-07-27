cb282540eac9c07719b10aaf00e9f98581f14237
rifatrakib <Rakib.1508@outlook.com>
2022-07-28 05:58:18 +0600

    routes added to access html page for property and building

--- realestate/urls.py
+++ realestate/urls.py
@@ -6,8 +6,12 @@ urlpatterns = [
     # property links
     path('realestate/property/<int:property_id>/json/', get_property_as_json, name='property_json'),
     path('realestate/property/<int:property_id>/xml/', get_property_as_xml, name='property_xml'),
+    path('realestate/property/', get_property_page, name='property_page'),
+    path('realestate/property/<int:page>/', get_property_page, name='property_page'),
     
     # building links
     path('realestate/building/<int:building_id>/json/', get_building_as_json, name='building_json'),
     path('realestate/building/<int:building_id>/xml/', get_building_as_xml, name='building_xml'),
+    path('realestate/building/', get_building_page, name='building_page'),
+    path('realestate/building/<int:page>/', get_building_page, name='building_page'),
 ]
