50dfdcf49add6d0a24f0c6c2523c61995db1d4aa
rifatrakib <Rakib.1508@outlook.com>
2022-08-17 18:18:50 +0600

    include property address and building models to the admin site

--- realestate/admin.py
+++ realestate/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from realestate.models import Property
+from realestate.models import Property, Building, PropertyAddress
 
 admin.site.register(Property)
+admin.site.register(Building)
+admin.site.register(PropertyAddress)
